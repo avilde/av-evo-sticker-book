285d47088b4654214d4e8a63a1bacf412aa86a65
Andris Vilde <vilde.andris@gmail.com>
2022-01-27 21:22:43 +0200

    Fix page preview and navigation

--- src/components/page/page/Page.tsx
+++ src/components/page/page/Page.tsx
@@ -28,17 +28,20 @@ export const PageComponent: React.FC<PageComponentProps> = ({
 	stickers,
 	logoSticker,
 }) => {
-	const [showPreview, setShowPreview] = React.useState<boolean>(false)
 	const isOdd = index % 2 === 0
+
+	const [showPreview, setShowPreview] = React.useState<boolean>(false)
+
 	const isPreviewEnabled =
 		(isOdd && currentPage === index - 1) ||
 		(!isOdd && currentPage === index)
 	const isTurned = currentPage >= index
-	const pageClasses = cn('page', {
+
+	const pageClasses = cn('page', 'pointer-events-none', {
 		isTurned: isTurned,
 		isOdd: isOdd,
 		isEven: !isOdd,
-		isHover: showPreview,
+		isPreviewEnabled: showPreview,
 	})
 
 	const pageStyle = {
@@ -47,14 +50,18 @@ export const PageComponent: React.FC<PageComponentProps> = ({
 	} as React.CSSProperties
 
 	const pageTurnerClasses = cn(
+		'pageTurner pointer-events-auto',
 		isOdd ? 'right-0' : 'left-0',
-		'bottom-0 top-0 border-4 border-indigo-500 w-12 h-full absolute cursor-pointer',
+		'bottom-0 top-0 w-20 h-full absolute cursor-pointer',
+		isOdd
+			? 'hover:bg-gradient-to-l from-black'
+			: 'hover:bg-gradient-to-r from-black',
 		{ isTurned: isTurned }
 	)
 
 	return (
 		<div className={pageClasses} style={pageStyle}>
-			<div className="stickerLayer">
+			<div className="stickerLayer select-none pointer-events-none">
 				{stickers?.map((sticker, stickerIndex) => (
 					<PageSticker
 						key={stickerIndex}
@@ -80,6 +87,16 @@ export const PageComponent: React.FC<PageComponentProps> = ({
 				onMouseEnter={() => isPreviewEnabled && setShowPreview(true)}
 				onMouseLeave={() => isPreviewEnabled && setShowPreview(false)}
 			></div>
+
+			<div
+				className={cn(
+					'absolute bottom-0 w-8 h-8 pb-2 select-none pointer-events-none',
+					'text-white  text-lg text-bold underline decoration-sky-500',
+					isOdd ? 'right-0 pl-2 text-left' : 'left-0 pr-2 text-right'
+				)}
+			>
+				{index}
+			</div>
 		</div>
 	)
 }
